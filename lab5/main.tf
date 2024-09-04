4170379f297c329cebe28f1884b7d9d39cbfa0f9
janakaw <janaka.seneviratne@gmail.com>
2024-09-05 02:27:48 +1000

    lab6: locals

--- lab5/main.tf
+++ lab5/main.tf
@@ -30,5 +30,38 @@ resource "aws_subnet" "variables-subnet" {
   }
 }
 
+locals {
+  team = "api_mgmt_dev"
+  application = "corp_api"
+  server_name = "ec2-${var.environment}-api-${var.variables_sub_az}"
+}
 
+# Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.variables-subnet.id
+  tags = {
+    Name = local.server_name
+    Owner = local.team
+    App = local.application
+  }
+}
 
--- lab5/variables.tf
+++ lab5/variables.tf
@@ -16,14 +16,23 @@ variable "vpc_cidr" {
 variable "variables_sub_cidr" {
   description = "CIDR Block for the Variables Subnet"
   type        = string
+  default     = "10.10.2.0/24"
 }
 
 variable "variables_sub_az" {
   description = "Availability Zone used Variables Subnet"
   type        = string
+  default     = "ap-southeast-2b"
 }
 
 variable "variables_sub_auto_ip" {
   description = "Set Automatic IP Assigment for Variables Subnet"
   type        = bool
+  default     = true
+}
+
+variable "environment" {
+  description = "ec2 env"
+  type        = string
+  default     = "dev"
 }
