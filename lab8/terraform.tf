97764eeeae4f89a4aa023243489b11cb5776c008
janakaw <janaka.seneviratne@gmail.com>
2024-09-08 07:00:27 +1000

    lab8: tls key gen and version fetch.

---
+++ lab8/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.9.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.6.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">=3.6"
+    }
+    http = {
+      source = "http"
+      version = "3.4.4"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.1"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  }
+}
